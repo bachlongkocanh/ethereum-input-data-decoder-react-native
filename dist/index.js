ddac09fc522895805ce07d575716f32f07cd01ef
HuuTho <huu.tho.ns@gmail.com>
2019-07-24 10:20:21 +0700

    Update for react native
    Change lib `fs` to `react-native-fs`

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var fs = require('fs');
+var fs = require('react-native-fs');
 var ethabi = require('ethereumjs-abi');
 var ethers = require('ethers');
 var Buffer = require('buffer/').Buffer;
